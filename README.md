4a9d89c471de459305c6c53d1fddbe2a65a5fcb4
wanghaoming <wanghaoming@ctfo.com>
2021-06-30 11:23:49 +0800

    add spring-native

---
+++ README.md
@@ -0,0 +1,5 @@
+## Relevant Articles:
+
+Module | Article
+--|--
+spring-native | [Introduction to Spring Native](https://www.baeldung.com/spring-native-intro)
---
+++ src/main/java/com/ymmihw/springnative/SpringNativeApp.java
@@ -0,0 +1,8 @@
+package com.ymmihw.springnative;
+
+public class SpringNativeApp {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World! This is a Spring Native Application");
+  }
+}
